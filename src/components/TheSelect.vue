d72f651c67f2adb1879f3dea73028433d223c236
양희영 <yangheeyoung@yanghuiyeong-ui-MacBookAir.local>
2023-04-12 21:54:02 +0900

    feat: minor version updated using dictionary

--- src/components/TheSelect.vue
+++ src/components/TheSelect.vue
@@ -8,18 +8,14 @@
         @change="($event) => selectHandler($event, idx)"
       >
         <option disabled selected>{{ group.title }}</option>
-        <!-- <option
+        <option
           v-for="option in group.options"
           :key="option"
           :disabled="checkNums(option, idx) === 0"
           :value="option"
         >
           {{ option }}
-          {{ checkNums(option, idx) ? `${checkNums(option, idx)}` : "(품절)" }}
-        </option> -->
-        <option v-for="option in group.options" :key="option" :value="option">
-          {{ option }}
-          {{ checkNums(option, idx) ? `${checkNums(option, idx)}` : "(품절)" }}
+          {{ checkNums(option, idx) === 0 ? "(품절)" : ""}}
         </option>
       </select>
     </div>
@@ -39,40 +35,19 @@ const resultData = await getSizeColorOptions();
 const selectData = resultData?.data;
 
 const selectedOptions = ref([]);
-const preSelectedList = ref(selectData.countList);
 const leftNumber = ref(0);
 
-const selectHandler = (selected, level) => {
-  let tmpSelected = selectedOptions.value;
-  if (tmpSelected.length === level) {
-    tmpSelected.push(selected);
-  } else if (tmpSelected.length > level) {
-    tmpSelected[level] = selected;
-    tmpSelected.slice(0, level + 1);
-  } else {
-    alert("선택 오류입니다.");
-  }
-  selectedOptions.value = tmpSelected;
-};
-
-const checkNums = (option, level) => {
-  let remainCnt = 0;
-  let tmpPreSel = preSelectedList.value;
-
-  return remainCnt;
-};
-
+// set Base Group Dictionary
 let defDict = { value: 0 };
-selectData.groupList.reverse().forEach((group) => {
+[...selectData.groupList].reverse().forEach((group) => {
   let newDict = {};
   group.options.forEach((option) => {
     newDict[option] = { ...defDict };
   });
-  defDict = { ...newDict, value: 0 }; // 1 depth 까지만 깊은복사가 되는 문제
+  defDict = { ...newDict, value: 0 };
 });
-console.log(defDict);
-defDict = JSON.parse(JSON.stringify(defDict)); // 임시로 JSON 함수 이용한 deepcopy... 왜 안되지?
 
+// add value to Base Group Dictionary
 selectData.countList.forEach((count) => {
   let tmpCombi = count.combination;
   let tmpCnt = count.remainCount;
@@ -81,12 +56,35 @@ selectData.countList.forEach((count) => {
     pastInnerDict.value += tmpCnt;
     let tmpCondition = tmpCombi[level];
     pastInnerDict = pastInnerDict[tmpCondition];
-    pastInnerDict.value += tmpCnt;
   }
 });
-console.log(defDict);
 
-// filter 함수로, 하나씩 추가
-// include 함수로, 통으로 비교?
-// 중간에 중간 옵션 바꿀 경우에도 바뀌어야지... 위에 옵션 바꾸면 그 아래 옵션 취소되어야 하나?
+// check value for each options
+const checkNums = (option, level) => {
+  let remainCnt = 0;
+  let tmpSelected = defDict;
+  let tmpSelOps = selectedOptions.value;
+  if (tmpSelOps.length >= level) {
+    for (let l = 0; l < level; l++) {
+      tmpSelected = tmpSelected[tmpSelOps[l]];
+    };
+    remainCnt = tmpSelected[option]?.value;
+  }
+  return remainCnt;
+};
+
+// handling select change
+const selectHandler = (selected, level) => {
+  let tmpSelOps = selectedOptions.value;
+  let tmpSelected = selected.target.value;
+  if (tmpSelOps.length === level) {
+    tmpSelOps.push(tmpSelected);
+  } else if (tmpSelOps.length > level) {
+    tmpSelOps[level] = tmpSelected;
+    tmpSelOps.slice(0, level + 1);
+  } else {
+    alert("선택 오류입니다.");
+  }
+  selectedOptions.value = tmpSelOps;
+};
 </script>
